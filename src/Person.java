e2a488b34469c5dab75ea7f4ee148064fda27ca3
AnthonyQuinn <AnthonyQuinn@AnthonyQuinn-PC>
2017-06-27 19:16:11 +0100

    This is sample text

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+
+public class Person {
+private String name;
+private int age;
+private boolean status;
+
+public Person (String myName,int myAge,boolean myStatus)
+{ name = myName;
+age = myAge;
+myStatus = true;
+}
+void changeName(String newName){
+	name = newName;
+}
+void changeAge(int newAge)
+{
+    age = newAge;	
+}
+}
+
