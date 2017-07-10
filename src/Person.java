806c2c8bb61cddc4ecc384e1ffb0df62f6156826
AnthonyQuinn <AnthonyQuinn@172.29.104.101>
2017-07-10 13:55:23 +0100

    changed printOut string from "status" to MyStatus

--- src/Person.java
+++ src/Person.java
@@ -19,7 +19,7 @@ void changeAge(int newAge)
 void printStates() {
     System.out.println("Name:" +
         name + " age:" + 
-        age + " status:" + status);
+        age + " myStatus:" + status);
 }
 }
 
